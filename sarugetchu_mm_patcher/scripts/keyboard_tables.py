8987d816b170e6e918c019cbcbd99d4709230f0b
Jasper Chan <jasperchan515@gmail.com>
2024-08-12 09:43:33 +0000

    keyboard_tables: print out mapping, add to encoding list

--- sarugetchu_mm_patcher/scripts/keyboard_tables.py
+++ sarugetchu_mm_patcher/scripts/keyboard_tables.py
@@ -12,8 +12,10 @@ keyboard_tables = [
         "維 緯 衣 違 遺 域 育 一 壱 逸 印 員 因 姻 引\n"
         "院 陰 隠 韻 飲 唯 易 音 芋 亥 伊 莞 斐 郁 磯\n"
         "猪 巌 允 胤",
-        "89E5 89E6 89E7 89E8 89E9 89EA 89EB 89EC 89ED 8CC4 9442 9443 9444 9445 9446 0A "
-        "9447 9448 9449 944A 944B 944C 944D 944E 944F 9450 9451 9452 9453 9454"
+        "89EE 89EF 89F0 89F1 89F2 89F3 89F4 89F5 89F6 89F7 89F8 89F9 89FA 89FB 89FC 0A "
+        "8A40 8A41 8A42 8A43 8A44 8A45 8A46 8A47 8A48 8A49 8A4A 8A4B 8A4C 8A4D 8A4E 0A "
+        "8A4F 8A50 8A51 8A52 8A53 936A 8A64 8A90 9455 9456 9457 9458 9459 945A 945B 0A "
+        "945C 945D 945E 945F"
     ),
     # う table 0
     (
@@ -24,15 +26,54 @@ keyboard_tables = [
     (
         "宮 影 衛 映 栄 永 泳 英 詠 鋭 易 液 疫 益 駅\n"
         "悦 謁 越 閲 円 園 塩 宴 延 援 沿 演 炎 煙 猿\n"
-        "縁 遠 鉛 会 依 回 恵 繧 役 慧 瑛 叡 苑\n",
+        "縁 遠 鉛 会 依 回 恵 繧 役 慧 瑛 叡 苑",
         "8A5A 8A5B 8A5C 8A5D 8A5E 8A5F 8A60 8A61 8A62 8A63 8A64 8A65 8A66 8A67 8A68 0A "
         "8A69 8A6A 8A6B 8A6C 8A6D 8A6E 8A6F 8A70 8A71 8A72 8A73 8A74 8A75 8A76 8A77 0A "
         "8A78 8A79 8A7A 8AB8 89F0 8AB9 8C4E 8AC8 935F 9464 9465 9466 9467"
-    )
+    ),
+    # お table 0
+    (
+        "汚 凹 央 奥 往 応 押 桜 横 欧 殴 王 翁 億 屋\n"
+        "憶 乙 恩 温 穏 音 和 悪 皇 黄 遠 卸 虞 於 旺\n"
+        "鳳 治",
+        "8A7B 8A7C 8A7D 8A7E 8A80 8A81 8A82 8A83 8A84 8A85 8A86 8A87 8A88 8A89 8A8A 0A "
+        "8A8B 8A8C 8A8D 8A8E 8A8F 8A90 93F9 89E8 8CD5 8CEC 8A79 9468 9469 946A 946B 0A "
+        "946C 946D"
+    ),
+    # か table 0
+    (
+        "下 化 仮 何 価 佳 加 可 夏 嫁 家 寡 暇 果 架\n"
+        "歌 河 渦 火 禍 科 稼 箇 花 荷 華 菓 蚊 課 貨\n"
+        "過 靴 画 我 芽 賀 雅 餓 介 会 回 塊 壊 快 怪\n"
+        "悔 懐 戒 拐 改 械 海 灰 界 皆 絵 解 開 階 劾\n"
+        "外 害 街 慨 概 涯 該 各 あ 拡 格 核 殻 獲 確\n"
+        "穫 覚 角 較 郭 閣 隔 革 学 岳 楽 額 割 喝 括",
+        "8A91 8A92 8A93 8A94 8A95 8A96 8A97 8A98 8A99 8A9A 8A9B 8A9C 8A9D 8A9E 8A9F 0A "
+        "8AA0 8AA1 8AA2 8AA3 8AA4 8AA5 8AA6 8AA7 8AA8 8AA9 8AAA 8AAB 8AAC 8AAD 8AAE 0A "
+        "8AAF 8AB0 8AB1 8AB2 8AB3 8AB4 8AB5 8AB6 8AB7 8AB8 8AB9 8ABA 8ABB 8ABC 8ABD 0A "
+        "8ABE 8ABF 8AC0 8AC1 8AC2 8AC3 8AC4 8AC5 8AC6 8AC7 8AC8 8AC9 8ACA 8ACB 8ACC 0A "
+        "8ACD 8ACE 8ACF 8AD0 8AD1 8AD2 8AD3 8AD4 8AD5 8AD6 8AD7 8AD8 8AD9 8ADA 8ADB 0A "
+        "8ADC 8ADD 8ADE 8ADF 8AE0 8AE1 8AE2 8AE3 8AE4 8AE5 8AE6 8AE7 8AE8 8AE9 8AEA"
+    ),
+    # か table 1
+    (
+        "活 渇 滑 褐 轄 乾 冠 刊 勘 勧 喚 堪 完 官 寒\n"
+        "寛 巻 干 幹 患 感 慣 憾 換 敢 肝 あ 款 歓 汗\n"
+        "あ 環 甘 監 看 管 簡 あ 缶 艦 あ あ 還 鑑 聞\n"
+        "閑 関 陥 館 丸 含 岸 岩 眼 あ 顔 願 客 合 月\n"
+        "甲 元 掛 且 垣 刈 株 潟 貝 伽 袈 あ 樺 魁 凱\n"
+        "楓 馨 あ あ 嵩 霞 叶 あ あ 茅 あ",
+        "8AEB 8AEC 8AED 8AEE 8AEF 8AF0 8AF1 8AF2 8AF3 8AF4 8AF5 8AF6 8AF7 8AF8 8AF9 0A "
+        "8AFA 8AFB 8AFC 8B40 8B41 8B42 8B43 8B44 8B45 8B46 8B47 8B48 8B49 8B4A 8B4B 0A "
+        "8B4C 8B4D 8B4E 8B4F 8B50 8B51 8B52 8B53 8B54 8B55 8B56 8B57 8B58 8B59 8B5A 0A "
+        "8B5B 8B5C 8B5D 8B5E 8B5F 8B60 8B61 8B62 8B63 8B64 8B65 8B66 8B9A 8CEF 8C6C 0A "
+        "8CD4 8C8B 946E 946F 9470 9471 9472 9473 9474 9475 9476 9477 9478 9479 947A 0A "
+        "947B 947C 947D 947E 9480 9481 9482 9483 9484 9485 9486"
+    ),
 
 ]
 
-
+prelim_table = {}
 
 for table in keyboard_tables:
     kanji, bin = table
@@ -42,5 +83,15 @@ for table in keyboard_tables:
     kanji_idx = 0
     bin_idx = 0
     for k, b in zip(kanji, bin):
-        print(b, k)
+        if k == "あ":
+            print(f"UNKNOWN: {b}")
+        else:
+            print(b, k)
+            prelim_table[b] = k
     print("")
+
+for i in range(0x89E5, 0x95FF + 1):
+    hex_str = hex(i)[2:].upper()
+    b1, b2 = hex_str[0:2], hex_str[2:4]
+    bstr = f'b"\\x{b1}\\x{b2}": "{prelim_table.get(hex_str, "??")}",'
+    print(bstr)
