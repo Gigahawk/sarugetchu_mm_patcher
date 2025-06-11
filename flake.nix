e57a95734e3a534ae59997cfd4eb77eaeb85d69a
Jasper Chan <jasperchan515@gmail.com>
2025-06-11 09:18:44 +0000

    flake: use explicit version

--- flake.nix
+++ flake.nix
@@ -197,7 +197,10 @@
 
         runHook postBuild
       '';
-      version = "1";
+      # https://discourse.nixos.org/t/how-to-create-a-timestamp-in-a-nix-expression/30329/2
+      lastModifiedStr = builtins.readFile "${pkgs.runCommand "timestamp" { when = self.lastModified; } "echo -n `date -d @$when +%Y-%m-%d_%H-%M-%S` > $out"}";
+      # https://discourse.nixos.org/t/flakes-accessing-selfs-revision/11237/8
+      version = "${toString (self.ref or self.shortRev or self.dirtyShortRev or "unknown")}_${lastModifiedStr}";
       resourceFiles = import ./resource-files.nix;
       resourceFilesStr = builtins.concatStringsSep "\n" resourceFiles;
       fontconfig_file = pkgs.makeFontsConf {
