c81697c804f3b66534b99904455ab64afaec0020
Jasper Chan <jasperchan515@gmail.com>
2025-07-08 14:01:42 -0700

    textures: switch to generated catch banner

---
+++ textures/catch_banner/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# ゲッチュ!!
+# In Ape Escape 3 protagonist says "Gotcha!"
+# Font appears to be the same as/very similar to the menu headers
+../build_header.sh \
+    -s "GOTCHA!!" \
+    -g east \
+    -b 4 \
+    -w 128 \
+    -h 32 \
+    -g west \
+    -y -2 \
+    --scale-factor 0.63 \
+    --font-size 42 \
